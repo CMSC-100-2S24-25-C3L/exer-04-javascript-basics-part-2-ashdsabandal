a0016fadedc2d864eb93bc9ede4dd964558d95e7
ashdsabandal <adsabandal@up.edu.ph>
2025-02-27 14:49:01 +0800

    edited test input: test.js

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 import { generateUniqueID, addAccount } from "./index.js";
 
 addAccount(["Lebron", "James", "lbj@a.com", 143])
+addAccount(["Lebron", "James", "lbj", 143])
+addAccount(["Lebron", "James", "lbj@a.com", 3])
+addAccount(["Kai", "Sotto", "lbj@a.com", 30])
