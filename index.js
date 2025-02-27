d3de252c105a4a47c35affc4a1431d63761d62fc
ashdsabandal <adsabandal@up.edu.ph>
2025-02-27 13:36:09 +0800

    added: index.js, test.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+COMMON JS
+module.exports.name = function
+const math = require("./math.js")
+
+ES MODULE
+import name from "./math.js"
+export default {add, mul, PI, obj}
+in package json, "type" : "module"
+
+*/
